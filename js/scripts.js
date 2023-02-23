3cab55c4dbea197afe1b908be652826cbede6d13
RosarioGebala <ro_gebala@hotmail.com>
2023-02-23 12:37:08 -0300

    up

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+document.getElementById('telefono').addEventListener('click', function () {
+    console.log("telefono");
+    document.getElementById('datos').innerHTML = "+54 9 11 12345"
+});
+
+document.getElementById('correo').addEventListener('click', function () {
+    console.log("correo");
+    document.getElementById('datos').innerHTML = "samgeba@gmail.com"
+});
+
+document.getElementById('instagram').addEventListener('click', function () {
+    console.log("instagram");
+    document.getElementById('datos').innerHTML = "@samgeba"
+});

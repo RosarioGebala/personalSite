acc00f4fa9703757ff39bc867b36e03725e655e5
RosarioGebala <113119029+RosarioGebala@users.noreply.github.com>
2023-05-10 13:54:07 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 document.getElementById('telefono').addEventListener('click', function () {
     console.log("telefono");
-    document.getElementById('datos').innerHTML = "+54 9 11 12345"
+    document.getElementById('datos').innerHTML = "+54 9 11 66035111"
 });
 
 document.getElementById('correo').addEventListener('click', function () {
     console.log("correo");
-    document.getElementById('datos').innerHTML = "samgeba@gmail.com"
+    document.getElementById('datos').innerHTML = "rosariogebala@gmail.com"
 });
 
-document.getElementById('instagram').addEventListener('click', function () {
-    console.log("instagram");
-    document.getElementById('datos').innerHTML = "@samgeba"
+document.getElementById('linkedin').addEventListener('click', function () {
+    console.log("linkedin");
+    document.getElementById('datos').innerHTML = "linkedin.com/in/rosariogebala"
 });
